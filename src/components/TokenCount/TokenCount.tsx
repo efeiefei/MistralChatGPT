acbd0f631e131b5077e8f0e60b026c823995db1d
efeiefei <efeigm@gmail.com>
2024-03-15 19:33:57 +0800

    Update TokenCount.tsx

--- src/components/TokenCount/TokenCount.tsx
+++ src/components/TokenCount/TokenCount.tsx
@@ -17,7 +17,7 @@ const TokenCount = React.memo(() => {
   const model = useStore((state) =>
     state.chats
       ? state.chats[state.currentChatIndex].config.model
-      : 'mistral-small'
+      : 'mistral-small-latest'
   );
 
   const cost = useMemo(() => {
